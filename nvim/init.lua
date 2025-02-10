dc7c545c9a7851d4768ab7456ddab03070eff02d
root <root@twohat>
2025-02-10 16:27:15 -0600

    added lsp and autocomplete to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,7 @@ vim.cmd([[
 
 
     set statusline+=\ %F\ %M\ %Y\ %R\ row\ %l\ col\ %c\
-    set guioptions=mlrb
+    "set guioptions=mlrb
     set showmode
     set number
 
@@ -79,6 +79,10 @@ require("lazy").setup({
         "neovim/nvim-lspconfig",
         "mfussenegger/nvim-lint",
         "mhartington/formatter.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/nvim-cmp",
+        "j-hui/fidget.nvim",
+        'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' },
         { "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
     },
     -- Configure any other settings here. See the documentation for more details.
@@ -88,10 +92,81 @@ require("lazy").setup({
     checker = { enabled = true },
 })
 
+-- lualine setup
+require("lualine").setup({})
+
+
+-- taken from https://lsp-zero.netlify.app/docs/getting-started.html
+
+-- Setup completion
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+vim.opt.signcolumn = 'yes'
+
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- This is where you enable features that only work
+-- if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
 
 -- Setup Mason
 require("mason").setup()
 require("mason-lspconfig").setup{
-    ensure_installed = { "lua_ls", "rust_analyzer"},
-    automatic_installation = true,
+    ensure_installed = { "lua_ls", "rust_analyzer","pylsp","java_language_server"},
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    }
 }
+
+
+---
+-- Autocompletion config
+---
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+})
